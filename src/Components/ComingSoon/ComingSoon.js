8f8f90320f0674aee9456f29f59fa2d91607ef91
Ranok-code <ranokraihan@gmail.com>
2021-06-13 21:29:53 +0600

    bug fixed

--- src/Components/ComingSoon/ComingSoon.js
+++ src/Components/ComingSoon/ComingSoon.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './ComingSoon.css';
 import workingGif from '../../images/monophy.gif'
 
+
 const ComingSoon = () => {
     return (
-        <div className='cs-container'>
+        <div className='cs-container animate__animated animate__zoomIn animate__faster'>
             <div className="cs-content">
                 <img src={workingGif} alt="" />
-                <h3>Working on it</h3>
+                <h3 >Working on it</h3>
                 <h1>Coming Soon</h1>
             </div>
         </div>
